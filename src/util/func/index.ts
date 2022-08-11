0babaae0bfb2508a7d978cbab5ad8e1462a03300
psh549633 <psh549633@gmail.com>
2022-08-11 15:55:18 +0900

    :hammer: :: 번역된 포켓몬 이름을 리턴하는 함수 작성

--- src/util/func/index.ts
+++ src/util/func/index.ts
@@ -1,2 +1,8 @@
+import translate from "../../../public/jsons/poke_lang.json";
+
 export const getRandomNumber = (start: number, end: number): number =>
   Math.floor(Math.random() * (end - start + 1) + start);
+
+export const translateName = (id: number, lang: string) => {
+  return (translate as any)[id][lang];
+};
