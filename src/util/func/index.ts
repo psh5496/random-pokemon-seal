280ff911b7f2b3c36f03760aede8ac54535a339f
psh549633 <psh549633@gmail.com>
2022-08-12 14:10:26 +0900

    :hammer: :: 남은 자리수만큼 0을 채워주는 함수 작성

--- src/util/func/index.ts
+++ src/util/func/index.ts
@@ -3,6 +3,9 @@ import translate from "../../../public/jsons/poke_lang.json";
 export const getRandomNumber = (start: number, end: number): number =>
   Math.floor(Math.random() * (end - start + 1) + start);
 
+export const fillZero = (str: string, width: number) =>
+  str.length >= width ? str : new Array(width - str.length + 1).join("0") + str;
+
 export const translateName = (id: number, lang: string) => {
   return (translate as any)[id][lang];
 };
